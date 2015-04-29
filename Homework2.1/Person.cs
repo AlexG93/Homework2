8ba81c157816bad5802ce566eb86818b4fcebdfe
AlexG93 <gingalau.alex@gmail.com>
2015-04-29 20:31:01 +0300

    reworked

---
+++ Homework2.1/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2._1
+{
+    class Person
+    {
+        protected string LastName;
+        protected string FirstName;
+        protected string DateofBirth;
+
+        public Person(string last, string first, string birth)
+        {
+            LastName = last;
+            FirstName = first;
+            DateofBirth = birth;
+        }
+        public Person()
+        {
+
+        }
+    }
+}
---
+++ Homework2.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee Alex = new Employee("Gingalau", "Alexandru", "Ianuarie", "Martie", "Milioane", 25);
+
+            Leave Concediu = new Leave("Decembrie", 232, "Concediu");
+            Alex.DisplayInfo();
+
+            try
+            {
+                Alex.AddNewLeave(Concediu, Alex);
+            }
+            catch (Exception)
+            {
+
+
+            };
+            Alex.DisplayInfo();
+            Console.ReadLine();
+
+
+        }
+    }
+}
